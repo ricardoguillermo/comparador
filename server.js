feece2e4094ddb7762017857271351130f3f7252
Ricardo Nario <diguibal@gmail.com>
2025-04-27 08:14:48 -0300

    casi listo falta buscador

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
-app.get('/canasta', (req, res) => {
-  const data = fs.readFileSync('public/canasta.json', 'utf-8');
+// Endpoint para servir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/canasta", (req, res) => {
+  const data = fs.readFileSync("public/canasta.json", "utf-8");
   res.json(JSON.parse(data));
 });
 
-app.post('/guardar', (req, res) => {
-  fs.writeFileSync('public/canasta.json', JSON.stringify(req.body, null, 2));
-  res.json({ mensaje: 'Guardado correctamente' });
+app.post("/guardar", (req, res) => {
+  fs.writeFileSync("public/canasta.json", JSON.stringify(req.body, null, 2));
+  res.json({ mensaje: "Guardado correctamente" });
 });
 
 app.listen(PORT, () => {
